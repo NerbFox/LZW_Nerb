0a932b0a2e37e196805160fd2137edc803118dfb
NerbFox <13521043@std.stei.itb.ac.id>
2023-07-10 00:04:50 +0700

    Update errorHandler.js

--- LZW_BE/middleware/errorHandler.js
+++ LZW_BE/middleware/errorHandler.js
@@ -1,36 +1,41 @@
-const {constants} = require('./constants');
+// const {constants} = require('./constants');
+// VALIDATION_ERROR: 400,
+// UNAUTHORIZED: 401,
+// FORBIDDEN: 403,
+// NOT_FOUND: 404,
+// INTERNAL_SERVER_ERROR: 500
 const errorHandler = (err, req, res, next) => {
     const statusCode = res.statusCode ? res.statusCode : 500;
     switch(statusCode){
-        case constants.VALIDATION_ERROR:
+        case 400:
             res.json({
                 title: "Validation Failed - Bad Request", 
                 message: err.message, 
                 stackTrace: err.stack
             });
             break;
-        case constants.UNAUTHORIZED:
+        case 401:
             res.json({
                 title: "Unauthorized",
                 message: err.message,
                 stackTrace: err.stack
             });
             break;
-        case constants.FORBIDDEN:
+        case 403:
             res.json({
                 title: "Forbidden",
                 message: err.message,
                 stackTrace: err.stack
             });
             break;
-        case constants.NOT_FOUND:
+        case 404:
             res.json({
                 title: "Not Found", 
                 message: err.message,
                 stackTrace: err.stack
             });
             break;
-        case constants.INTERNAL_SERVER_ERROR:
+        case 500:
             res.json({
                 title: "Internal Server Error",
                 message: err.message,
